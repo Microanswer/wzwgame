dc3ecba936a922fe846036e7bec555867ac7185a
Microanswer <microanswer@outlook.com>
2021-01-04 00:49:20 +0800

    方块克隆游戏实现

--- build/build.js
+++ build/build.js
@@ -2,12 +2,15 @@ let getWebpackCfgOf = require("./webpack_config");
 let webpack = require("webpack");
 
 console.log("开始 webpack 打包...")
-webpack(getWebpackCfgOf("production"), function (err, stat) {
+
+webpack(getWebpackCfgOf("production"), function (err, /**@type {webpack.Stats}*/stat) {
     if (err) {
         console.log("webpack 打包出错：");
         console.error(err);
     } else {
         console.log("webpack 打包结束。");
-        console.log(stat);
+        console.log(stat.toString(/**@type {Stats.ToStringOptions}*/{
+            colors: true
+        }));
     }
 });
