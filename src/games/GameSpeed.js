35ba1d24e4e62014b3d70d6514bd7c10320fcd26
Microanswer <microanswer@outlook.com>
2021-05-28 18:25:19 +0800

    加入赛车游戏2

--- src/games/GameSpeed.js
+++ src/games/GameSpeed.js
@@ -1,3 +1,88 @@
+let { WzwScreen } = require("../platform/WzwScreen");
+
+const NAME_ME = "me";
+const NAME_OTHER = "other";
+
+// 急速模式下，时间长度。
+const TURBO_TINESPACE = 30;
+
+// 每关卡的时间长度
+const LEVEL_TIMESPACE = [350, 300, 250, 200, 150, 100, 50];
+
+/**
+ * 赛车内
+ * @param name
+ * @param offsetRow
+ * @param offsetCol
+ * @param wzwScreen {WzwScreen}
+ * @constructor
+ */
+function Car(name, offsetRow, offsetCol, wzwScreen) {
+    this.atoms = [
+        [0,1,0],
+        [1,1,1],
+        [0,1,0],
+        [1,1,1]
+    ];
+
+    this.offsetCol = offsetCol;
+    this.offsetRow = offsetRow;
+    this.wzwScreen = wzwScreen;
+    this.name = name;
+}
+
+Car.prototype.setOnOutScreenListener = function (listener) {
+    this.outScreenListener = listener;
+};
+
+Car.prototype.update = function () {
+    if (this.name === NAME_OTHER) {
+        this.offsetRow = this.offsetRow + 1;
+    }
+
+    if (this.offsetRow > this.wzwScreen.option.atomRowCount) {
+        if (this.outScreenListener) {
+            this.outScreenListener(this);
+        }
+    }
+};
+Car.prototype.render = function (atoms) {
+    WzwScreen.mergeArr(this.atoms, atoms, this.offsetRow, this.offsetCol, undefined);
+};
+
+
+/**
+ *
+ * @param wzwScreen {WzwScreen}
+ * @constructor
+ */
+function Road(wzwScreen) {
+    this.left = [];
+    this.right = [];
+
+    let flag = 1; // 0 输出无， 1,2 输出有。
+    for (let i = 0; i < wzwScreen; i++) {
+        if (i % 2 === 0) {
+            this.left.push([flag > 0 ? 1 : 0]);
+            this.right.push([flag > 0 ? 1 : 0]);
+        }
+
+        if (flag + 1 > 2) {
+            flag = 0;
+        } else {
+            flag = flag + 1;
+        }
+    }
+}
+
+Road.prototype.render = function (atoms) {
+    WzwScreen.mergeArr(this.left, atoms, 0, 0, undefined);
+    WzwScreen.mergeArr(this.right, atoms, 0, this.wzwScreen.option.atomColCount - 1, undefined);
+
+    let l = this.left.pop();this.left.splice(0, 0, l);
+    let c = this.right.pop();this.right.splice(0, 0, c);
+};
+
 /**
  * 赛车游戏
  * @constructor
